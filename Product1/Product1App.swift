5a3b382079c5ab55d175c24d83d56983e7a09f74
Mohammad Shaikh <ms18shai@siena.edu>
2023-06-19 10:26:58 -0400

    Initial Commit

---
+++ Product1/Product1App.swift
@@ -0,0 +1,17 @@
+//
+//  Product1App.swift
+//  Product1
+//
+//  Created by Shaikh, Mohammad on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Product1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
