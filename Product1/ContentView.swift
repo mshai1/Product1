d724a2f75a6e465c083c85dfccc93bc27ccc61e3
Mohammad Shaikh <ms18shai@siena.edu>
2023-06-22 15:42:34 -0400

    updated

--- Product1/ContentView.swift
+++ Product1/ContentView.swift
@@ -8,14 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    let mainColor = Color(red:20/255, green:28/255, blue:58/255)
+    let accentColor = Color(red:48/255, green:105/255, blue:240/255)
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            mainColor.ignoresSafeArea()
+            VStack{
+                Text("1/10")
+                    .font(.callout)
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                Text("What was the first computer bug?")
+                    .font(.largeTitle)
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                Spacer()
+                HStack{
+                    Button(action: {
+                        print("Tapped on Choice 1")
+                    }, label: {
+                        Text("Ant")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    Button(action: {
+                        print("Tapped on Choice 2")
+                    }, label: {
+                        Text("Beetle")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    Button(action: {
+                        print("Tapped on Choice 3")
+                    }, label: {
+                        Text("Moth")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    Button(action: {
+                        print("Tapped on Choice 4")
+                    }, label: {
+                        Text("Fly")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                }
+            }
         }
-        .padding()
+        .foregroundColor(.white)
     }
 }
 
@@ -23,4 +75,76 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+    
+    
+    struct ContentView: View {
+        let question = Question(questionText: "What was the first computer bug?", possibleAnswers: ["Ant", "Beetle", "Moth", "Fly"], correctAnswerIndex: 2)
+        
+        let mainColor = Color(red:20/255, green: 28/255, blue: 58/255)
+        let accentColor = Color(red:48/255, green: 105/255,blue: 240/255)
+        
+        var body: some View{
+            ZStack{
+                mainColor.ignoresSafeArea()
+                VStack{
+                    Text("1/10")
+                        .font(.callout)
+                        .multilineTextAlignment(.leading)
+                        .padding()
+                    Text(question.questionText)
+                        .font(.largeTitle)
+                        .bold()
+                        .multilineTextAlignment(.leading)
+                }
+            
+            HStack{
+                
+                Button(action: {
+                    print("Tapped on Choice 1")
+                }, label: {
+                    Text(question.possibleAnswers[0])
+                        .font(.body)
+                        .bold()
+                        .multilineTextAlignment(.center)
+                        .padding()
+                        .border(accentColor, width: 4)
+                })
+                Button(action: {
+                    print("Tapped on Choice 2")
+                }, label: {
+                    Text(question.possibleAnswers[1])
+                        .font(.body)
+                        .bold()
+                        .multilineTextAlignment(.center)
+                        .padding()
+                        .border(accentColor, width: 4)
+                })
+                Button(action: {
+                    print("Tapped on Choice 3")
+                }, label: {
+                    Text(question.possibleAnswers[2])
+                        .font(.body)
+                        .bold()
+                        .multilineTextAlignment(.center)
+                        .padding()
+                        .border(accentColor, width: 4)
+                })
+                Button(action: {
+                    print("Tapped on Choice 4")
+                }, label: {
+                    Text(question.possibleAnswers[3])
+                        .font(.body)
+                        .bold()
+                        .multilineTextAlignment(.center)
+                        .padding()
+                        .border(accentColor, width: 4)
+                })
+            }
+        }
+            
+        }
+    }
+    
 }
+    
+
