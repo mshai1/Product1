b16543f510823f2df7f32fc59c242de4bed37bc2
Mohammad <mshai1>
2023-06-26 21:37:22 -0400

    updated the Trivia update with navigation

---
+++ Product1/Models/Views/WelcomeUI.swift
@@ -0,0 +1,52 @@
+//
+//  WelcomeUI.swift
+//  Product1
+//
+//  Created by Mohammad Shaikh on 6/26/23.
+//
+
+import SwiftUI
+
+struct WelcomeUI: View {
+    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
+      let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    var body: some View {
+        NavigationView{
+            ZStack {
+                mainColor.ignoresSafeArea()
+                VStack {
+                    Spacer()
+                                       VStack(alignment: .leading, spacing: 0) {
+                                           Text("Select the correct answers to the following questions.")
+                                               .font(.largeTitle)
+                                               .bold()
+                                               .multilineTextAlignment(.leading)
+                                               .padding()
+                                       }
+                                       Spacer()
+                                       NavigationLink(
+                                           destination: GameView(),
+                                           label: {
+                                               HStack {
+                                                   Spacer()
+                                                   Text("Okay, let's go!")
+                                                       .font(.body)
+                                                       .bold()
+                                                       .padding()
+                                                   Spacer()
+                                               }.background(accentColor)
+                                           })
+                                   }
+                                   .foregroundColor(.white)
+                }
+                .foregroundColor(.white)
+            }
+        }
+    }
+
+
+struct WelcomeUI_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeUI()
+    }
+}
--- Product1/Product1App.swift
+++ Product1/Product1App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Product1App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            WelcomeUI()
         }
     }
 }
